a4a51c89b6fa506425fa34b12d9df18994ab1aad
Himanshu Sharma <himanshush214@gmail.com>
2020-04-07 12:03:16 +0530

    Updated

--- Design Patterns/Singleton/DbSingletonDemo.java
+++ Design Patterns/Singleton/DbSingletonDemo.java
@@ -1,19 +1,10 @@
 import java.util.Arrays;
 public class DbSingletonDemo{
-  private static String[] zipCodeList = {"203205","250003"};
   public static void main(String[] args) {
 
     DbSingleton instance = DbSingleton.getInstance();
 
     System.out.println(instance);
-
-    boolean test = Arrays.asList(zipCodeList).contains("203205");
-      if(test){
-        System.out.println("Fine");
-      }
-      else{
-        System.out.println("Not fine");
-      }
   }
 
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # This Repository Contains **Low Level Design** Concepts,Theory and Code Snippets.
 ___
-### There is Folder called *Desing Patterns*, It contains theory and implementation of design pattern in java.
+### There is Folder called *Desing Patterns* , It contains theory and implementation of various design pattern in java.
 
 ### `Will update this repo time to time`
